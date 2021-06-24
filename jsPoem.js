d5e55154be270096fcb20675cb5ab1a3c9f0fd14
Ayo-Fatoye <78175442+Ayo-Fatoye@users.noreply.github.com>
2021-06-24 11:06:22 -0500

    Create jsPoem.js
    Made a poem object

---
+++ jsPoem.js
@@ -0,0 +1,4 @@
+const poem = {
+	header: 'This is an amateur\'s poem.',
+	body: 'This is the first line of my poem\nIsn\'t it wonderful?\nI don\'t know how to write a poem\nyet in my humiliating inexperience,\nhere I am\nwriting a poem\nand i\'m ducking enjoying it :)
+};
